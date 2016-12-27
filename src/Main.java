049fcacb917064261aa65856ad24fa0b20cdbe94
Devin Morgan <devinm@mit.edu>
2016-12-26 21:40:15 -0500

    Added a custom ErrorListener to determine when syntax errors ocurr in the scanner

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import grammar.scanner.JavaScanner;
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.*;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.BitSet;
 import java.util.Scanner;
 
 
@@ -21,54 +22,35 @@ import java.util.Scanner;
 
 class Main {
 
-//    private static void runFile(String filePath) {
-//        // get the fileExtension
-//        String fileExtension = "none";
-//        int i = filePath.lastIndexOf('.');
-//        if (i > 0) {
-//            fileExtension = filePath.substring(i+1);
-//        }
-//
-//        // run the test in the file
-//        if (fileExtension.equals("dcf")) {
-//            try {
-//                System.out.println("\n\n" + filePath);
-//                CharStream stream = new ANTLRFileStream(filePath);
-//                Scanner lexer = new Scanner(stream);
-//                TokenStream tokens = new CommonTokenStream(lexer);
-//                DecafParser parser = new DecafParser(tokens);
-//                ParseTree tree = parser.program();
-//                ParseTreeWalker walker = new ParseTreeWalker();
-//                DecafListener listener = new DecafListener();
-//                walker.walk(listener, tree);
-////            Trees.inspect(tree, parser);
-//            }
-//            catch (IOException e) {
-//                System.out.println("There was an error:\n" + e);
-//            }
-//        }
-//    }
-
     private static void runScannerTests() {
-        /*
-            Get each of the input files from tests/scanner/input
-            for file in directory:
-                run the file on the Antlr Scanner
-                collect the output from the Antlr Scanner
-                print the output
-        */
 
         // get all the tests for the scanner
         String scannerTestsDirectory = "tests/scanner/input";
 
         // run each scanner test
-        File[] files = new File(scannerTestsDirectory).listFiles();
+        File root = new File(scannerTestsDirectory);
+        File[] files = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return !file.isHidden();
+            }
+        });
+
+
         for (File file : files) {
+
+            // print file name
+            System.out.println(file.getPath());
 
             try {
                 CharStream stream = new ANTLRFileStream(file.getPath());
                 JavaScanner scanner = new JavaScanner(stream);
                 PrintStream outputStream = System.out;
+
+                // use the custom ErrorListener to detect syntax errors from Scanner
+                ANTLRErrorListener errorListener = new ScannerErrorListener(outputStream);
+                scanner.removeErrorListeners();
+                scanner.addErrorListener(errorListener);
 
                 // iterate over every token from the stream
                 for (Token token = scanner.nextToken(); token.getType() != JavaScanner.EOF; token = scanner.nextToken()) {
@@ -76,37 +58,72 @@ class Main {
                     String text = token.getText();
 
                     // collect relevant data about the token in a human readable format
-                    switch (token.getType()) {
-                        case JavaScanner.INT:
-                            type = " INTLITERAL";
-                            break;
-                        case JavaScanner.STRING:
-                            type = " STRINGLITERAL";
-                            break;
-                        case JavaScanner.BOOL:
-                            type = " BOOLEANLITERAL";
-                            break;
-                        case JavaScanner.ID:
-                            type = " IDENTIFIER";
-                            break;
+                    if (token.getType() == JavaScanner.INT) {
+                        type = " INT";
+                    }
+                    else if (token.getType() == JavaScanner.STRING) {
+                        type = " STRING";
+                    }
+                    else if (token.getType() == JavaScanner.BOOL) {
+                        type = " BOOL";
+                    }
+                    else if (token.getType() == JavaScanner.ID) {
+                        type = " IDENT";
+                    }
+                    else if (token.getType() >= JavaScanner.RES_CLASS && token.getType() <= JavaScanner.RES_NEW) {
+                        type = "RES_WORD";
                     }
 
                     // aggregate this data in the outputStream to help debugging
-                    outputStream.println(token.getLine()  + type + " " + text);
+                    outputStream.println("line " + token.getLine() + ":" + token.getCharPositionInLine() +
+                            " " + type + " " + text);
                 }
             }
             catch (IOException e) {
-                System.out.println("There was an error:\n" + e);
-                // print the error:
-                System.err.println(file.toString() + " " + e);
-                //scanner.skip(); // replaces
+                System.err.println("There was an error: " + file.toString() + " " + e);
                 System.exit(1);
             }
+            catch (NoViableAltException e) {
+                System.err.println("There was an error: " + file.toString() + " " + e);
+                System.exit(1);
+            }
+
+            System.out.println("\n\n");
         }
     }
 
     public static void main(String[] args) {
         Main.runScannerTests();
+    }
+
+    private static class ScannerErrorListener implements ANTLRErrorListener {
+
+        private final PrintStream printStream;
+
+        public ScannerErrorListener(PrintStream printStream) {
+            this.printStream = printStream;
+        }
+
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
+            this.printStream.println("line " + i + ":" + i1 + "  " + s);
+        }
+
+        @Override
+        public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+
+        }
+
+        @Override
+        public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+
+        }
+
+        @Override
+        public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+
+        }
+
     }
 }
 
