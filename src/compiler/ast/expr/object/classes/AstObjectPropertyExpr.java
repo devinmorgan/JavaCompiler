86e82c9bc3c681e58c93be3d0e77bc5b9458f46a
Devin Morgan <devinm@mit.edu>
2017-01-07 19:46:36 -0500

    re-organized and re-named files

--- src/compiler/ast/expr/object/classes/AstObjectPropertyExpr.java
+++ src/compiler/ast/expr/object/classes/AstObjectPropertyExpr.java
@@ -3,19 +3,7 @@ package compiler.ast.expr;
 /**
  * Created by devinmorgan on 1/5/17.
  */
-public class AstObjectProperty extends AstExpr {
+public class AstObjectPropertyExpr extends AstExpr {
     final AstExpr object;
     final AstExpr property;
 }
-
-
-
-
-
-
-
-/*
- FINISH FROM expr array_call #OjbectMethodCall
- ...
- to RES_NULL
- */
