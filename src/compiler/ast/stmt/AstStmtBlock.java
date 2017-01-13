3a3a9566a0f72829519960abc1eee1ca04093b5a
Devin Morgan <devinm@mit.edu>
2017-01-13 14:27:31 -0500

    Removed unecessary code from performSemanticAnalysis()

--- src/compiler/ast/stmt/AstStmtBlock.java
+++ src/compiler/ast/stmt/AstStmtBlock.java
@@ -1,6 +1,9 @@
 package compiler.ast.stmt;
 
 import compiler.ast.Ast;
+import compiler.ast.decl.AstVarDecl;
+import compiler.symbol_table.SymbolTable;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.util.ArrayList;
 
@@ -13,5 +16,12 @@ public class AstStmtBlock extends AstStmt {
     public AstStmtBlock(int line, int col, ArrayList<Ast> blockContents) {
         super(line, col);
         this.blockContents = blockContents;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        for (Ast ast : this.blockContents) {
+            ast.performSemanticAnalysis(environment, errorMessage);
+        }
     }
 }
