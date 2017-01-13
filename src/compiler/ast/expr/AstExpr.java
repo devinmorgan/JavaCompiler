7dc425ed0fbc6947f4e253369e9314c0c66237e7
Devin Morgan <devinm@mit.edu>
2017-01-13 16:14:31 -0500

    Added the custom runtime exception BinaryExpressionTypeMismatch()

--- src/compiler/ast/expr/AstExpr.java
+++ src/compiler/ast/expr/AstExpr.java
@@ -1,12 +1,21 @@
 package compiler.ast.expr;
 
 import compiler.ast.Ast;
+import compiler.ast.type.AstType;
 
 /**
  * Created by devinmorgan on 1/5/17.
  */
-public class AstExpr extends Ast {
+public abstract class AstExpr extends Ast {
     public AstExpr(int line, int col) {
         super(line, col);
+    }
+
+    public abstract AstType getType();
+
+    public class BinaryExpressionTypeMismatch extends RuntimeException {
+        public BinaryExpressionTypeMismatch(AstType lhsType, AstType rhsType) {
+            super("LHS is of type " + lhsType.toString() + " but RHS is of type " + rhsType.toString());
+        }
     }
 }
