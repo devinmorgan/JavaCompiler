489b7bd4f6d1d2962d5fc1b6580f09e5d1f81092
Devin Morgan <devinm@mit.edu>
2017-01-13 14:56:35 -0500

    Included the import of SymbolTable and updated performSemanticAnalysis

--- src/compiler/ast/stmt/ctrl_flow/AstNonVoidReturnStmt.java
+++ src/compiler/ast/stmt/ctrl_flow/AstNonVoidReturnStmt.java
@@ -2,6 +2,7 @@ package compiler.ast.stmt.ctrl_flow;
 
 import compiler.ast.expr.AstExpr;
 import compiler.ast.stmt.AstStmt;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/5/17.
@@ -12,5 +13,19 @@ public class AstNonVoidReturnStmt extends AstStmt {
     public AstNonVoidReturnStmt(AstExpr returnValue) {
         super(returnValue.getLine(), returnValue.getCol());
         this.returnValue = returnValue;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // semantically validate the return expression
+        this.returnValue.performSemanticAnalysis(environment, errorMessage);
+
+        // verify that the type of the return expression matches the method type
+        if (! environment.getCurrentScopeReturnType().equals(this.returnValue.getType())) {
+            String message = "Type of return value " + this.returnValue.getType()
+                    + " does not match return type from the method signature "
+                    + environment.getCurrentScopeReturnType() + ". Line " + this.getLine();
+            errorMessage.append(message);
+        }
     }
 }
--- src/compiler/ast/stmt/ctrl_flow/AstVoidReturnStmt.java
+++ src/compiler/ast/stmt/ctrl_flow/AstVoidReturnStmt.java
@@ -1,6 +1,7 @@
 package compiler.ast.stmt.ctrl_flow;
 
 import compiler.ast.stmt.AstStmt;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/5/17.
@@ -8,5 +9,10 @@ import compiler.ast.stmt.AstStmt;
 public class AstVoidReturnStmt extends AstStmt {
     public AstVoidReturnStmt(int line, int col) {
         super(line, col);
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        return;
     }
 }
