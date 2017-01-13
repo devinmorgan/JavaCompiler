f68b47420c0dc5c2c3ce6df0590bf62f4afbe11a
Devin Morgan <devinm@mit.edu>
2017-01-13 14:20:02 -0500

    added the correct import for the SymbolTable

--- src/compiler/ast/stmt/AstExprStmt.java
+++ src/compiler/ast/stmt/AstExprStmt.java
@@ -1,6 +1,7 @@
 package compiler.ast.stmt;
 
 import compiler.ast.expr.AstExpr;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/5/17.
@@ -11,5 +12,10 @@ public class AstExprStmt extends AstStmt {
     public AstExprStmt(AstExpr expr) {
         super(expr.getLine(), expr.getCol());
         this.expr = expr;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        this.expr.performSemanticAnalysis(environment, errorMessage);
     }
 }
