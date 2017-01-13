1f676147eb55e5f4fb6fd9c7679815109da5807f
Devin Morgan <devinm@mit.edu>
2017-01-13 13:12:33 -0500

    Added a comment

--- src/compiler/ast/lists/AstArgsList.java
+++ src/compiler/ast/lists/AstArgsList.java
@@ -2,6 +2,7 @@ package compiler.ast.lists;
 
 import compiler.ast.Ast;
 import compiler.ast.AstArg;
+import compiler.symbol_table.SymbolTable;
 
 import java.util.ArrayList;
 
@@ -17,5 +18,13 @@ public class AstArgsList extends Ast{
 
     public void addArg(AstArg arg) {
         this.argsList.add(arg);
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // validate each arg individually
+        for (AstArg arg : this.argsList) {
+            arg.performSemanticAnalysis(environment, errorMessage);
+        }
     }
 }
