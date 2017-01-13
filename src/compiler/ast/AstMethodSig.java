faa62dd3c11dac1643649c4881933cfb9e66e9df
Devin Morgan <devinm@mit.edu>
2017-01-13 13:10:49 -0500

    Updated performSemanticAnalysis() to add and then remove a new local scope.

--- src/compiler/ast/AstMethodSig.java
+++ src/compiler/ast/AstMethodSig.java
@@ -3,6 +3,7 @@ package compiler.ast;
 import compiler.ast.Ast;
 import compiler.ast.lists.AstParamsList;
 import compiler.ast.type.AstType;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/7/17.
@@ -19,13 +20,24 @@ public class AstMethodSig extends Ast {
         this.params = params;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public AstParamsList getParams() {
+        return this.params;
+    }
+
+    public AstType getReturnType() {
+        return this.returnType;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof AstMethodSig) {
             AstMethodSig that = (AstMethodSig) obj;
 
-            return this.returnType.equals(that.returnType)
-                    && this.name.equals(that.name)
+            return this.name.equals(that.name)
                     && this.params.equals(that.params);
         }
         return false;
@@ -33,6 +45,21 @@ public class AstMethodSig extends Ast {
 
     @Override
     public int hashCode() {
-        return this.returnType.hashCode() * this.name.hashCode();
+        return this.name.hashCode() * this.params.hashCode();
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // create a new local scope
+        environment.pushNewScope();
+
+        // check validity of returnType
+        this.returnType.performSemanticAnalysis(environment, errorMessage);
+
+        // check the validity or the parameters
+        this.params.performSemanticAnalysis(environment, errorMessage);
+
+        // remove the current local scope
+        environment.popLocalScope();
     }
 }
