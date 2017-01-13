9cc1b2ad747cd2cc470522f4222605c2ef400403
Devin Morgan <devinm@mit.edu>
2017-01-13 16:48:47 -0500

    Created custom runtime exception UnknownVariableTypeException and added methods performSemanticAnalysis and getType

--- src/compiler/ast/expr/AstVarLocationExpr.java
+++ src/compiler/ast/expr/AstVarLocationExpr.java
@@ -1,15 +1,45 @@
 package compiler.ast.expr;
 
 import compiler.ast.expr.AstExpr;
+import compiler.ast.type.AstType;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/7/17.
  */
 public class AstVarLocationExpr extends AstExpr {
     final String varName;
+    private AstType varType;
 
     public AstVarLocationExpr(int line, int col, String varName) {
         super(line, col);
         this.varName = varName;
+    }
+
+    @Override
+    public AstType getType() {
+        if (this.varType == null) {
+            throw new UnknownVariableTypeException(this.varName);
+        }
+        return this.varType;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // grab this variables type from the environtment
+        this.varType = environment.lookupVariableType(this.varName);
+
+        // verify that the variable has been declared (i.e. it should have a type)
+        if (this.varType == null) {
+            String message = "Variable " + this.varName
+                    + " is used before it is has been declared. Line: " + this.getLine() + "\n";
+            errorMessage.append(message);
+        }
+    }
+
+    private class UnknownVariableTypeException extends RuntimeException {
+        UnknownVariableTypeException(String varName) {
+            super("Variable " + varName + " has an unknown type or has not been declared");
+        }
     }
 }
