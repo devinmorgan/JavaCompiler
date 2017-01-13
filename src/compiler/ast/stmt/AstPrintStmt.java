2655b2e6e61b2d033be09518be111f9bfa7eaf79
Devin Morgan <devinm@mit.edu>
2017-01-13 14:27:55 -0500

    Updated preformSemanticAnalysis()

--- src/compiler/ast/stmt/AstPrintStmt.java
+++ src/compiler/ast/stmt/AstPrintStmt.java
@@ -2,6 +2,10 @@ package compiler.ast.stmt;
 
 import compiler.ast.expr.AstExpr;
 import compiler.ast.stmt.AstStmt;
+import compiler.ast.type.AstBoolType;
+import compiler.ast.type.AstIntType;
+import compiler.ast.type.AstStringType;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/5/17.
@@ -12,5 +16,20 @@ public class AstPrintStmt extends AstStmt{
     public AstPrintStmt(int line, int col, AstExpr arg) {
         super(line, col);
         this.arg = arg;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // validate the argument
+        this.arg.performSemanticAnalysis(environment, errorMessage);
+
+        // print stmts can only accept ints, booleans, or Strings
+        if (!(this.arg.getType() instanceof AstIntType)
+                && !(this.arg.getType() instanceof AstBoolType)
+                && !(this.arg.getType() instanceof AstStringType)) {
+            String message = "Invalid argument for print(). Valid arguments must be int, " +
+                    "boolean, or String. Line: " + this.arg.getLine() + "\n";
+            errorMessage.append(message);
+        }
     }
 }
