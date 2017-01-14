bb58777cd156a90fa6966bc4d66c4d03ce8a0986
Devin Morgan <devinm@mit.edu>
2017-01-13 22:27:12 -0500

    Added the getType() and the performSemanticAnalysis() methods

--- src/compiler/ast/expr/unary/bool/AstNotExpr.java
+++ src/compiler/ast/expr/unary/bool/AstNotExpr.java
@@ -2,6 +2,9 @@ package compiler.ast.expr.unary.bool;
 
 import compiler.ast.expr.AstExpr;
 import compiler.ast.expr.unary.AstUnaryExpr;
+import compiler.ast.type.AstBoolType;
+import compiler.ast.type.AstType;
+import compiler.symbol_table.SymbolTable;
 
 /**
  * Created by devinmorgan on 1/5/17.
@@ -9,5 +12,28 @@ import compiler.ast.expr.unary.AstUnaryExpr;
 public class AstNotExpr extends AstUnaryExpr {
     public AstNotExpr(int line, int col, AstExpr arg) {
         super(line, col, arg);
+    }
+
+    @Override
+    public AstType getType() {
+        // verify that the type of the expression is boolean
+        AstBoolType booleanType = new AstBoolType(-1, -1);
+        if (! this.arg.getType().equals(booleanType)) {
+            throw new UnaryExpressionArgumentMismatch(booleanType, this.arg.getType());
+        }
+        return booleanType;
+    }
+
+    @Override
+    public void performSemanticAnalysis(SymbolTable environment, StringBuilder errorMessage) {
+        // semantically validate the argExpr
+        this.arg.performSemanticAnalysis(environment, errorMessage);
+
+        // verify that the argExpr is a boolean type
+        if (! this.arg.getType().equals(new AstBoolType(-1, -1))) {
+            String message = "The ! operator requires a boolean type but found "
+                    + this.arg.getType() + ". Line: " + this.getLine() +  "\n";
+            errorMessage.append(message);
+        }
     }
 }
