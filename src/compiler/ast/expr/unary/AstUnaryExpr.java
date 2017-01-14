3d24c89d1cd7fe3a64ac094e0578d3e0d99747e5
Devin Morgan <devinm@mit.edu>
2017-01-13 22:26:27 -0500

    Added a the custom exception UnaryExpresssionArgumentMismatch

--- src/compiler/ast/expr/unary/AstUnaryExpr.java
+++ src/compiler/ast/expr/unary/AstUnaryExpr.java
@@ -1,15 +1,22 @@
 package compiler.ast.expr.unary;
 
 import compiler.ast.expr.AstExpr;
+import compiler.ast.type.AstType;
 
 /**
  * Created by devinmorgan on 1/5/17.
  */
 public abstract class AstUnaryExpr extends AstExpr{
-    final AstExpr arg;
+    public final AstExpr arg;
 
     public AstUnaryExpr(int line, int col, AstExpr arg) {
         super(line, col);
         this.arg = arg;
+    }
+
+    public class UnaryExpressionArgumentMismatch extends RuntimeException {
+        public UnaryExpressionArgumentMismatch(AstType expectedType, AstType foundType) {
+            super("Expected type  " + expectedType + " but found type " + foundType + "\n");
+        }
     }
 }
